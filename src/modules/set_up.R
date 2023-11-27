4e59f145d5e01155de0f19722e91a047e40ce15b
Tien-Cheng <39258210+Illustratien@users.noreply.github.com>
2023-11-27 15:57:44 +0100

    update the packages requirements

--- src/modules/set_up.R
+++ src/modules/set_up.R
@@ -1,9 +1,12 @@
 pkg.list <- 
   c("XML","purrr", "ggforce",   
-    "dplyr", "moments","ROI.plugin.glpk","ompr.roi","ompr", 
-    "rlang" ,"tidyr", "pacman" ,   
+    "dplyr", "moments",
+    # "ROI.plugin.glpk","ompr.roi","ompr", 
+    "rlang" ,"tidyr", "pacman" ,  "ggbeeswarm","sma",
     "parallel","doParallel","broom")
 local.pkg <- installed.packages()[,"Package"]
 new.packages <- pkg.list[!(pkg.list %in% local.pkg)]
 if(length(new.packages)) install.packages(new.packages)
 devtools::install_github("Illustratien/toolPhD",dependencies = T)
+update.packages("dplyr")
+update.packages("purrr")
